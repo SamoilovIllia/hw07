b36a402732bd7ac261eef97c62f3ce3997144f16
SamoilovIllia <samoilov.illia.a@gmail.com>
2023-03-12 15:57:52 +0100

    Add file to work
    Добавил файлы для работы

---
+++ normalize6.py
@@ -0,0 +1,21 @@
+import re
+
+CYRILLIC_SYMBOLS = 'абвгдеёзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+# print(TRANS)
+
+
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)
+    return t_name
+
+
+if __name__ == '__main__':
+    pass
